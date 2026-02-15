2d63d67e777750d8ed1224700b750108ba403224
EF-dataprojects <contactcahayainti@gmail.com>
2026-02-15 07:05:06 +0100

    Mise à jour portfolio propre

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
-const accordions = document.querySelectorAll(".accordion-header");
+const toggle = document.getElementById("themeToggle");
+const body = document.body;
 
-accordions.forEach(header => {
-  header.addEventListener("click", () => {
-    const content = header.nextElementSibling;
-    const plus = header.querySelector("span");
-    if(content.style.display === "block") {
-      content.style.display = "none";
-      plus.textContent = "+";
-    } else {
-      content.style.display = "block";
-      plus.textContent = "-";
+toggle.addEventListener("click", () => {
+  body.classList.toggle("light");
+  toggle.textContent = body.classList.contains("light") ? "🌞" : "🌙";
+});
+
+// scroll reveal
+const reveals = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("active");
     }
+  });
+}, { threshold: 0.2 });
+
+reveals.forEach(el => observer.observe(el));
+
+document.querySelectorAll(".project-link").forEach(card => {
+  card.addEventListener("click", () => {
+    window.location.href = card.dataset.link;
   });
 });
